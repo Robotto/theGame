5026e555ae428cf715f5322002e8da4cad940c7a
robotto <mark@moore.dk>
2020-04-01 10:18:58 +0200

    Added highscore with persistence through file read/write. Tak Emil!

--- main.py
+++ main.py
@@ -21,7 +21,14 @@ terrain=[] #unused, for now...
 enemies=[]
 shots=[]
 
-
+highScore=0
+try:
+    with open('highScoreFile') as file:
+        data = file.read()
+        highScore=int(data.strip())
+        print("Loaded highscore:",highScore)
+except:
+    print("highScoreFile not found, resetting to 0.")
 
 screen = pygame.display.set_mode((gameWindowWidth, gameWindowHeight))
 
@@ -97,6 +104,8 @@ while not done:
                 playerObject.points +=1
                 enemy.playSound()
                 #print('Points:',playerObject.points)
+                if playerObject.points > highScore:
+                    highScore = playerObject.points
         if collisionChecker(enemy,playerObject):
             playerObject.collisionSFX.play()
             print("OUCH!")
@@ -113,8 +122,10 @@ while not done:
 
     #Score:                                                 antialias?, color
     text = font.render('SCORE: ' + str(playerObject.points), True,(0, 255, 0))
-    textRect = text.get_rect() #REKT!
-    screen.blit(text,textRect)
+    screen.blit(text,(0,0))
+
+    text = font.render('HIGHSCORE: ' + str(highScore), True, (255, 0, 0))
+    screen.blit(text, (300,0))
 
     for shot in shots:
         shot.draw()
@@ -126,3 +137,7 @@ while not done:
     clock.tick(60)
 
 
+#When done is false the while loop above exits, and this code is run:
+with open('highScoreFile', 'w') as file:
+    print("Saving highscore to file:", highScore)
+    file.write(str(highScore))
